3b13eefc59b1f80d9ab2b2359838e48cd5ced0dd
Conthorcon <94369676+Conthorcon@users.noreply.github.com>
2022-06-14 19:34:38 +0700

    Add files via upload

---
+++ Cmdl.h
@@ -0,0 +1,5 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+void CommandLine3(string algorithm,int input_size,string output_parameter);
---
+++ Sort.h
@@ -0,0 +1,7 @@
+void ShellSort(int a[],int n,int &ccmp);
+void FlashSort(int a[],int n,int &ccmp);
+void CountingSort(int a[],int n,int &ccmp);
+
+void ShellSort(int a[],int n);
+void FlashSort(int a[],int n);
+void CountingSort(int a[],int n);
