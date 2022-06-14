026f0a5d1f41583bad9c66342af86c6d66c16a5d
Conthorcon <94369676+Conthorcon@users.noreply.github.com>
2022-06-14 19:43:34 +0700

    Delete Sort.h

--- Sort.h
+++
@@ -1,7 +0,0 @@
-void ShellSort(int a[],int n,int &ccmp);
-void FlashSort(int a[],int n,int &ccmp);
-void CountingSort(int a[],int n,int &ccmp);
-
-void ShellSort(int a[],int n);
-void FlashSort(int a[],int n);
-void CountingSort(int a[],int n);
